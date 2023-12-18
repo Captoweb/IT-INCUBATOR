7e580a0ace02744da102d7a2c22459abf97a4968
Captoweb <captainphp@yandex.ru>
2023-12-18 16:55:58 +0300

    first day

---
+++ js/app.js
@@ -0,0 +1,70 @@
+// Решение задач Freecodecamp
+// 18 декабря 2023 года
+
+// 1 Comment Your JavaScript Code
+// simple comment
+
+/**
+ * long comment
+ */
+
+
+// 2 Declare JavaScript Variables
+var myName;
+
+// 3 Storing Values with the Assignment Operator
+var a;
+a = 7;
+
+
+// 4 Assigning the Value of One Variable to Another
+a = 7;
+var b;
+
+b = a;
+
+// 5 Initializing Variables with the Assignment Operator
+var a = 9;
+
+
+// 6 Declare String Variables
+var myFirstName = 'Ivan';
+var myLastName = 'Ivanov';
+
+// 7 Understanding Uninitialized Variables
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+
+// 8 Understanding Case Sensitivity in Variables
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+
+// 9 Explore Differences Between the var and let Keywords
+let catName = "Oliver";
+let catSound = "Meow!";
+
+
+// 10 Declare a Read-Only Variable with the const Keyword
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
+
+// 11 Add Two Numbers with JavaScript
+const sum = 10 + 10;
+
