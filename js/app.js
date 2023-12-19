b7d3c755353d78f6fee685c3e930d795ef21388e
Captoweb <captainphp@yandex.ru>
2023-12-20 00:44:57 +0300

    Second day

--- js/app.js
+++ js/app.js
@@ -68,3 +68,120 @@ console.log(FCC, fact); // Change this line
 // 11 Add Two Numbers with JavaScript
 const sum = 10 + 10;
 
+
+// 19 декабря
+// 12 Subtract One Number from Another with JavaScript
+const difference = 45 - 33;
+
+
+// 13 Multiply Two Numbers with JavaScript
+const product = 8 * 10;
+
+
+// 14 Divide One Number by Another with JavaScript
+const quotient = 66 / 33;
+
+
+// 15 Increment a Number with JavaScript
+let myVar = 87;
+myVar++;
+
+// 16 Decrement a Number with JavaScript
+let myVar = 11;
+myVar--;
+
+// 17 Create Decimal Numbers with JavaScript
+const ourDecimal = 5.7;
+const myDecimal = 5.7;
+
+
+// 18 Multiply Two Decimals with JavaScript
+const product = 2.0 * 2.5;
+
+
+
+// 19 Divide One Decimal by Another with JavaScript
+const quotient = 4.4 / 2.0; // Change this line
+
+
+// 20 Finding a Remainder in JavaScript
+const remainder = 11 % 3;
+
+
+// 21 Compound Assignment With Augmented Addition
+let a = 3;
+let b = 17;
+let c = 12;
+
+a += 12;
+b += 9;
+c += 7;
+
+
+// 22 Compound Assignment With Augmented Subtraction
+let a = 11;
+let b = 9;
+let c = 3;
+
+a -= 6;
+b -= 15;
+c -= 1;
+
+
+// 23 Compound Assignment With Augmented Multiplication
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+a *= 5;
+b *= 3;
+c *= 10;
+
+
+// 24 Compound Assignment With Augmented Division
+let a = 48;
+let b = 108;
+let c = 33;
+
+a /= 12;
+b /= 4;
+c /= 11;
+
+
+// 25 Escaping Literal Quotes in Strings
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; 
+
+
+
+// 26 Quoting Strings with Single Quotes
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+
+
+// 27 Escape Sequences in Strings
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
+
+
+
+// 28 Concatenating Strings with Plus Operator
+const myStr = "This is the start. " + "This is the end.";
+
+
+
+// 29 Concatenating Strings with the Plus Equals Operator
+let myStr = "This is the first sentence. ";
+myStr += "This is the second sentence."
+
+
+// 30 Constructing Strings with Variables
+const myName = "Stanislav";
+const myStr = "My name is" + myName + "and  and I am well!";
+
+
+
+
+
+
+
+
+
--- js/codewars.js
+++ js/codewars.js
@@ -1,3 +1,4 @@
+// 18 декабря
 // Returning Strings
 //Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
 // [Make sure you type the exact thing I wrote or the program may not execute properly]
@@ -16,3 +17,38 @@ function multiply(number){
     if(number < 0) return (number * 5**(number.toString().length -1)); 
     else return (number * 5**number.toString().length);
 }
+
+
+// 19 декабря
+
+// BASIC: Making Six Toast
+function sixToast(num) {
+    if (num > 6) return (6 - num) * -1
+    else return 6 - num
+}
+
+
+
+// Check same case
+function sameCase(a, b){
+    if ( a.toLowerCase() == a.toUpperCase() || b.toLowerCase() == b.toUpperCase() ) {
+        return -1
+    }
+     if ( a.toLowerCase() == a && b.toLowerCase() == b || a.toUpperCase() == a && b.toUpperCase() == b ) {
+        return 1
+    }
+     return 0
+}
+
+
+// Create Phone Number
+function createPhoneNumber(numbers){
+    numbers.splice(0,0,'(');
+    numbers.splice(4,0,')');
+    numbers.splice(5,0, ' ');
+    numbers.splice(9,0, '-');
+   
+   return numbers.join('');
+  }
+
+  
