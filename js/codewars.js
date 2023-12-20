c55a5a3289c2a709e84a0a96130d55f204b02b2e
Captoweb <captainphp@yandex.ru>
2023-12-21 01:18:05 +0300

    Third day

--- js/codewars.js
+++ js/codewars.js
@@ -49,6 +49,115 @@ function createPhoneNumber(numbers){
     numbers.splice(9,0, '-');
    
    return numbers.join('');
+}
+
+
+
+
+// 20 декабря
+
+// Switch it Up!
+function switchItUp(number){
+    switch(number) {
+        case 0:
+          return 'Zero'
+        break;
+        case 1:
+            return 'One'
+        break;
+        case 2:
+            return 'Two'
+        break;
+        case 3:
+            return 'Three'
+        break;
+        case 4:
+            return 'Four'
+        break;
+        case 5:
+            return 'Five'
+        break;  
+        case 6:
+            return 'Six'
+        break;
+        case 7:
+            return 'Seven'
+        break;
+        case 8:
+           return 'Eight'
+        break;
+        case 9:
+            return 'Nine'
+        break;
+    }
+     
+  }
+  
+  
+
+  // The highest profit wins!
+  function minMax(arr){
+    let newArr = []
+  
+    let min = arr[0]
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i]
+        }
+    }
+    
+    let max = arr[0]
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i]
+        }
+    }
+    newArr[0] = min
+    newArr[1] = max
+    
+    return newArr
   }
 
+
+// Square(n) Sum
+function squareSum(numbers){
+    let sum = 0
+    numbers.map(item => {
+      sum+= item * item
+    })
+    return sum
+}
+
+
+
+// Difference of Volumes of Cuboids
+function findDifference(a, b) {
+    let kub1 = 1
+    let kub2 = 1
+    a.map(item => kub1 *= item)
+    b.map(item => kub2 *= item)
+    let res = kub1 - kub2
+    if (res < 0) res = res * (-1)
+    return res
+}
+
+
+
+// A wolf in sheep's clothing
+function warnTheSheep(queue) {
+
+    queue = queue.reverse()
+     if (queue[0] == "wolf")	{
+         return "Pls go away and stop eating my sheep"
+     }
   
+    for (let i = 0; i < queue.length; i++) {
+        if (queue[i] == "wolf") {
+             return `Oi! Sheep number ${i}! You are about to be eaten by a wolf!`
+            // console.log('Oi! Sheep number', i ,'! You are about to be eaten by a wolf!')
+        }  
+     }
+   
+  }
+
+
